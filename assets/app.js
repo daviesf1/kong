14e83b9ff5c506c488835f6caec6bce9d257a726
thefosk <marco@mashape.com>
2015-03-02 16:41:42 -0800

    Update 2015-03-03T00:41:39.997Z

--- assets/app.js
+++ assets/app.js
@@ -3,6 +3,15 @@
     var $navbar = $($(this).data('target'));
     $navbar.slideToggle(150);
   });
+
+  $('.toggle-page-section').on('click', function (e) {
+    e.preventDefault();
+    var $link = $(this);
+
+    $link.parent().next('.page-section').stop().slideToggle(300, function () {
+      $link.toggleClass('active');
+    });
+  });
 }(jQuery));
 
 //# sourceMappingURL=maps/app.js.map
